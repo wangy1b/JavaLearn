342a08de44a92a89a3ebe94e7470167317926c3e
wangy1b <wangy1b@163.com>
2020-07-07 11:32:50 +0800

    add at ul

---
+++ src/main/java/RotateString.java
@@ -0,0 +1,74 @@
+// 左右字符串交换
+public class RotateString {
+    public  static String rotate1(String s,int leftSize) {
+        if (leftSize <=0 || leftSize >= s.length()) {
+            return s;
+        }
+        return process1(s.toCharArray(), 0, leftSize - 1, s.length() - 1);
+    }
+    //如果认为str[L..M]是做左部分，str[M+1..R]是右部分
+    //请原地调整，只能用几个有限变量，左->右，右->左
+    public static String process1(char[] str, int L, int M, int R){
+        reverse(str, L, M);
+        reverse(str, M + 1, R);
+        reverse(str, L, R);
+        return String.valueOf(str);
+    }
+
+    public static void reverse(char[] str, int L, int R) {
+        while (L < R) {
+            char tmp = str[L];
+            str[L++] = str[R];
+            str[R--] = tmp;
+        }
+    }
+
+    public static String rotate2(String s, int leftSize) {
+        if (leftSize <=0 || leftSize >= s.length()) {
+            return s;
+        }
+        char[] str = s.toCharArray();
+        int L = 0;
+        int R = str.length - 1;
+        int lpart = leftSize;
+        int rpart = str.length - leftSize;
+        int same = Math.min(lpart, rpart);
+        int diff = lpart - rpart;
+        exchange(str, L, R, same);
+        while (diff != 0 ){
+            if(diff > 0) { //左边大
+                L += same;
+                lpart = diff;
+            } else { //右边大
+                R -= same;
+                rpart = -diff;
+            }
+            same = Math.min(lpart,rpart);
+            diff = lpart - rpart;
+            exchange(str, L, R, same);
+        }
+        return String.valueOf(str);
+    }
+    // str[l..]数出size大小，和str[..r]数出size大小，来交换
+    public static void exchange(char[] chas, int start, int end, int size){
+        int i = end - size + 1;
+        char tmp = 0;
+        while (size-- !=0) {
+            tmp = chas[start];
+            chas[start] = chas[i];
+            chas[i] = tmp;
+            start++;
+            i++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String test_str = "abcdefgh";
+        int cut_size = 2;
+        String test_result1 = rotate1(test_str, cut_size);
+        System.out.println(test_result1);
+        String test_result2 = rotate2(test_str, cut_size);
+        System.out.println(test_result2);
+    }
+}
---
+++ src/main/java/testJsonArray.java
@@ -0,0 +1,96 @@
+import com.alibaba.fastjson.JSONArray;
+//import org.junit.Test;
+
+import java.io.*;
+import java.util.List;
+
+public class testJsonArray {
+
+    public static List<String> expStr(String str){
+        List<String> exObj= JSONArray.parseArray(str,String.class);
+        return exObj;
+    }
+
+    public static String readFile(File file) {
+    try {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        // 把每次读取的内容写入到内存中，然后从内存中获取
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        // 只要没读完，不断的读取
+        while ((len = fileInputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, len);
+        }
+        // 得到内存中写入的所有数据
+        byte[] data = outputStream.toByteArray();
+        fileInputStream.close();
+        return new String(data,"utf-8");
+//            return new String(data,"utf-8");
+        //return new String(data, "GBK");//以GBK（什么编码格式）方式转
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+//    @Test
+    public  void test() {
+
+//            String a = "[20180117,4402,27,5428505,8532298,5398490,96,\"137105,111\",20181023,0.000,1.000,0.000,32.900,32.900,1.000,\"2018-10-26 01:24:50.383\",181026004618618,20.4000,201801,28.120]";
+//            List<String> test = expStr(a);
+//            System.out.println(test);
+////            test.forEach(System.out.println(_));
+//            test.forEach(t -> System.out.println(t));
+
+
+        String filename = "C:\\Users\\32006\\IdeaProjects\\test\\src\\main\\resources\\ps_002_test.csv";
+
+        File file = new File(filename);
+        BufferedReader reader = null;
+        try {
+//                System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            int line = 1;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                System.out.println("line " + line + ": " + tempString);
+                List<String> test = expStr(tempString);
+//                    test.forEach(t -> System.out.println(t));
+                line++;
+                if ( line > 2 ) {
+                    break;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+
+
+//            String data = readFile(new File(filename));
+//            System.out.println(data);
+//
+//            List<String> test = expStr(data.trim());
+//            test.forEach(t -> System.out.println(t));
+
+
+
+
+    }
+}
+
+
+
+
