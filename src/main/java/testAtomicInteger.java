21b0a90658a2faa6f9c0f8689a06336e5b49f91e
wangy1b <wangy1b@163.com>
2020-07-07 11:32:34 +0800

    add at ul

---
+++ src/main/java/testAtomicInteger.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class testAtomicInteger {
+
+        private static final int THREADS_COUNT = 2;
+
+        public static int count = 0;
+        public static volatile int countVolatile = 0;
+        public static AtomicInteger atomicInteger = new AtomicInteger(0);
+        public static CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        public static synchronized void increase() {
+
+                count++;
+                countVolatile++;
+                atomicInteger.incrementAndGet();
+
+        }
+
+        public static void main(String[] args) throws InterruptedException {
+            Thread[] threads = new Thread[THREADS_COUNT];
+            for(int i = 0; i< threads.length; i++) {
+                threads[i] = new Thread(() -> {
+                    for(int i1 = 0; i1 < 1000; i1++) {
+                        increase();
+                    }
+                    countDownLatch.countDown();
+                });
+                threads[i].start();
+            }
+
+            countDownLatch.await();
+
+            System.out.println("count:"+count);
+            System.out.println("countVolatile:"+countVolatile);
+            System.out.println("atomicInteger:"+atomicInteger.get());
+        }
+
+
+
+}
