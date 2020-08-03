0ffc983e9911ce1015eb9d2cdf1b4c7dbbe65650
wangy1b <wangy1b@163.com>
2020-08-03 11:09:42 +0800

    leetcode
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/MaxTurbulenceSize.java
+++ src/main/java/com/wyb/leetcode/MaxTurbulenceSize.java
@@ -2,16 +2,26 @@ package com.wyb.leetcode;
 
 public class MaxTurbulenceSize {
     public static void main(String[] args) {
-        int[] nums = {9,4,2,10,7,8,8,1,9};
+        int[] nums = {9, 4, 2, 10, 7, 8, 8, 1, 9};
+        // int[] nums = {4, 8, 12, 16};
         System.out.println(maxTurbulenceSize(nums));
     }
-
+/*
+ 不会
+ */
     private static int maxTurbulenceSize(int[] nums) {
-        if (nums.length == 1) return 1;
+        int N = nums.length;
+        int ans = 1;
+        int anchor = 0;
 
-
-        for (int i = 1; i < nums.length; i=i+2) {
+        for (int i = 1; i < N; ++i) {
+            int c = Integer.compare(nums[i - 1], nums[i]);
+            if (i == N - 1 || c * Integer.compare(nums[i], nums[i + 1]) != -1) {
+                if (c != 0) ans = Math.max(ans, i - anchor + 1);
+                anchor = i;
+            }
         }
-        return 0;
+
+        return ans;
     }
 }
