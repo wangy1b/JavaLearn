0c5320219797fcc1bbd70b7593979808a27ee112
wangy1b <wangy1b@163.com>
2020-08-06 18:59:02 +0800

    todo
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/MaxProfitII.java
+++ src/main/java/com/wyb/leetcode/MaxProfitII.java
@@ -7,24 +7,25 @@ public class MaxProfitII {
         // int[] prices = {2,4,1};
         // int[] prices = {1,2,3,4,5};
         // int[] prices = {7, 6, 4, 3, 1};
-        int[] prices = {6, 1, 3, 2, 4, 7};
+        // int[] prices = {6, 1, 3, 2, 4, 7};
+        // int[] prices = {3, 2, 6, 5, 0, 3};
+        int[] prices = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfitII(prices));
     }
 
 
     /**
-     *  todo 剩下的部分step需要灵活变动
-     *
+     * todo 剩下的部分step需要灵活变动
      */
     private static int maxProfitII(int[] prices) {
         int maxprofit = 0;
         // for (int idx = 0; idx < prices.length; idx++) {
-            int idx = 1;
-            for (int step = 1; step <= prices.length - idx - 1; step++) {
-                maxprofit = Math.max(maxprofit, findCurrentMaxProfit(prices, idx, step));
-                System.out.println("idx=" + idx + " step=" + step + " maxprofit=" + findCurrentMaxProfit(prices, idx, step));
-            }
-            // System.out.println("idx = " + idx +" maxprofit = " + findCurrentMaxProfit(prices,1, 3));
+        int idx = 1;
+        for (int step = 1; step <= prices.length - idx - 1; step++) {
+            maxprofit = Math.max(maxprofit, findCurrentMaxProfit(prices, idx, step));
+            // System.out.println("idx=" + idx + " step=" + step + " maxprofit=" + findCurrentMaxProfit(prices, idx, step));
+        }
+        // System.out.println("idx = " + idx +" maxprofit = " + findCurrentMaxProfit(prices,1, 3));
         // }
         // System.out.println("step = 3 :" + findCurrentMaxProfit(prices,1, 3));
         // System.out.println("maxprofit :" + maxprofit);
@@ -36,9 +37,15 @@ public class MaxProfitII {
         if (idx + step > prices.length) {
             return 0;
         }
-        for (int i = idx + step; i < prices.length; i++) {
-            if (prices[i] > prices[idx]) {
-                return prices[i] - prices[idx] + findCurrentMaxProfit(prices, i + step, step);
+        System.out.println(String.format("start at %s step %s", idx, step));
+        for (int end = idx + step; end < prices.length; end++) {
+            if (prices[end] > prices[idx]) {
+                int temp_max = 0;
+                for (int j = 1; j < prices.length - idx + step; j++) {
+                    // temp_max = Math.max(temp_max, findCurrentMaxProfit(prices, end + j + 1, j));
+                    temp_max = Math.max(temp_max, findCurrentMaxProfit(prices, end + j, j));
+                }
+                return prices[end] - prices[idx] + temp_max;
             }
         }
 
