dbbab4ed9ca9fdb15d794fe94bdf67b453f809ea
wangy1b <wangy1b@163.com>
2020-08-13 11:10:41 +0800

    palindrome
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/ValidPalindrome.java
+++ src/main/java/com/wyb/leetcode/ValidPalindrome.java
@@ -3,18 +3,23 @@ package com.wyb.leetcode;
 public class ValidPalindrome {
 
     public static void main(String[] args) {
-        String s = "A man, a plan, a canal: Panama";
+        // String s = "A man, a plan, a canal: Panama";
+        String s = "a12, b   B , 21 a + - / | ";
+        System.out.println(s.replaceAll("\\P{LD}+", "").toLowerCase());
         System.out.println(isPalindrome(s));
     }
 
 
     private static boolean isPalindrome(String s) {
-        if (s.isEmpty()) return true;
-        s.replaceAll("(,|:)","");
-        System.out.println(s);
-
-
-        return false;
+        boolean res = true;
+        if (s.isEmpty()) return res;
+        StringBuffer ns = new StringBuffer();
+        ns.append(s.replaceAll("\\P{LD}+", "").toLowerCase());
+        int start = 0, end = ns.length() -1;
+        while (start < end){
+            if (ns.charAt(start++) != ns.charAt(end--)) res = false;
+        }
+        return res;
     }
 
 
