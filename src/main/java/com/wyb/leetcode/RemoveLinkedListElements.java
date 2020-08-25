595cee2d9b558508bb75d9909637b13290beff7c
wangy1b <wangy1b@163.com>
2020-08-25 14:40:48 +0800

    RemoveLinkedListElements
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/RemoveLinkedListElements.java
+++ src/main/java/com/wyb/leetcode/RemoveLinkedListElements.java
@@ -1,9 +1,18 @@
 package com.wyb.leetcode;
+/*
 
+203. 移除链表元素
+删除链表中等于给定值 val 的所有节点。
+
+示例:
+输入: 1->2->6->3->4->5->6, val = 6
+输出: 1->2->3->4->5
+
+ */
 public class RemoveLinkedListElements {
     public static void main(String[] args) {
         // int[] nums = {1,2,6,3,4,5,6};
-        int[] nums = {6,6,1};
+        int[] nums = {};
         ListNode listNode = ListNode.generateListNode(nums);
         ListNode res = removeElements(listNode,6);
         ListNode.printListNode(res);
@@ -11,15 +20,17 @@ public class RemoveLinkedListElements {
 
     private static ListNode removeElements(ListNode head, int val) {
         if (head == null) return null;
-        ListNode p = head;
-        while (p != null ) {
+        ListNode newList = new ListNode(Integer.MIN_VALUE);
+        newList.next = head;
+        ListNode p = newList;
+        while (p.next != null ) {
             ListNode t = p.next;
-            if (t.val == val && t != null) {
+            if (t.val == val) {
                 p.next = t.next;
-            }
-            p = p.next;
+            } else
+                p = p.next;
         }
-        return head;
+        return newList.next;
     }
 
 }
