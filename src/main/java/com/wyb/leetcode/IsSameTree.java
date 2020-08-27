a1a4dc154b07918f0b56f7bb077939971192b8bd
wangy1b <wangy1b@163.com>
2020-08-27 22:39:14 +0800

    add link

--- src/main/java/com/wyb/leetcode/IsSameTree.java
+++ src/main/java/com/wyb/leetcode/IsSameTree.java
@@ -2,6 +2,16 @@ package com.wyb.leetcode;
 
 import java.util.ArrayDeque;
 
+/*
+
+100. 相同的树
+给定两个二叉树，编写一个函数来检验它们是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+https://leetcode-cn.com/problems/same-tree/
+
+ */
 public class IsSameTree{
 
     public static void main(String[] args) {
