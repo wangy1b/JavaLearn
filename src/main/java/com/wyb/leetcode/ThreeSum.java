1dbee8111a615a0e61a1c41a2132556738ed0ce1
wangy1b <wangy1b@163.com>
2020-08-28 11:28:54 +0800

    ThreeSum
    Signed-off-by: wangy1b <wangy1b@163.com>

---
+++ src/main/java/com/wyb/leetcode/ThreeSum.java
@@ -0,0 +1,100 @@
+package com.wyb.leetcode;
+
+import java.util.*;
+
+/*
+
+15. 三数之和
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+示例：
+给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+ */
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+    // 常规做法超时
+    private static List<List<Integer>> threeSum(int[] nums) {
+        List res = new LinkedList();
+        Set sets = new HashSet();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                for (int k = j+1; k <  nums.length; k++) {
+                    int tmp = nums[i] + nums[j] + nums[k];
+                    if ( tmp == 0 ){
+                        List<Integer> tmpList = new LinkedList<>();
+                        Set tmpSet = new HashSet();
+
+                        tmpList.add(nums[i]);
+                        tmpList.add(nums[j]);
+                        tmpList.add(nums[k]);
+
+                        tmpSet.add(nums[i]);
+                        tmpSet.add(nums[j]);
+                        tmpSet.add(nums[k]);
+                        if (!sets.contains(tmpSet)){
+                            res.add(tmpList);
+                            sets.add(tmpSet);
+                        }
+                    }
+                }
+
+            }
+        }
+        return res;
+    }
+
+    // 懒得看了
+    private static List<List<Integer>> threeSumOfficial(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 枚举 a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                // 需要和上一次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+
+    }
