92aa1ecc8327b7ac7579378348758414f0c7cc78
wangy1b <wangy1b@163.com>
2020-08-29 18:50:46 +0800

    queue + list

--- src/main/java/com/wyb/leetcode/BinaryTreePostorderTraversal.java
+++ src/main/java/com/wyb/leetcode/BinaryTreePostorderTraversal.java
@@ -25,15 +25,21 @@ https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
  */
 public class BinaryTreePostorderTraversal {
 
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(1,new TreeNode(2,new TreeNode(3),new TreeNode(4)),new TreeNode(5));
+        System.out.println(postorderTraversal(treeNode));
+        System.out.println(postorderTraversal2(treeNode));
+    }
+
     // 递归
-    public List<Integer> postorderTraversal1(TreeNode root) {
+    private static List<Integer> postorderTraversal1(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         helper(root,res);
         return res;
     }
     // 后序遍历
     //  左 -> 右 -> 中
-    private void helper(TreeNode root,List<Integer> res) {
+    private static void helper(TreeNode root,List<Integer> res) {
         if (root != null) {
             if (root.left != null) {
                 helper(root.left,res);
@@ -47,7 +53,8 @@ public class BinaryTreePostorderTraversal {
 
 
     // 迭代
-    public List<Integer> postorderTraversal(TreeNode root) {
+    // stack + list
+    private static List<Integer> postorderTraversal(TreeNode root) {
         LinkedList<TreeNode> stack = new LinkedList<>();
         LinkedList<Integer> output = new LinkedList<>();
         if (root == null) {
@@ -56,6 +63,7 @@ public class BinaryTreePostorderTraversal {
 
         stack.add(root);
         while (!stack.isEmpty()) {
+            // removes the last element of this list,
             TreeNode node = stack.pollLast();
             output.addFirst(node.val);
             if (node.left != null) {
@@ -64,6 +72,30 @@ public class BinaryTreePostorderTraversal {
             if (node.right != null) {
                 stack.add(node.right);
             }
+        }
+        return output;
+    }
+
+    // queue + list
+    private static  List<Integer> postorderTraversal2(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            // removes the head (first element) of this list.
+            TreeNode node = queue.remove();
+            output.addFirst(node.val);
+            if (node.left != null) {
+                queue.addFirst(node.left);
+            }
+            if (node.right != null) {
+                queue.addFirst(node.right);
+            }
+
         }
         return output;
     }
