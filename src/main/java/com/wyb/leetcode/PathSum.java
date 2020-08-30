87694a8343c6f41c505c9aeea6f182e6ea324a5c
wangy1b <wangy1b@163.com>
2020-08-30 22:17:44 +0800

    rename to PathSum

--- src/main/java/com/wyb/leetcode/PathSum.java
+++ src/main/java/com/wyb/leetcode/PathSum.java
@@ -24,7 +24,7 @@ package com.wyb.leetcode;
 
  */
 
-public class HasPathSum {
+public class PathSum {
 
     public static void main(String[] args) {
         TreeNode treeNode = new TreeNode(3,new TreeNode(9),new TreeNode(20,new TreeNode(15),new TreeNode(7)));
