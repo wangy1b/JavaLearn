e48339657e18177774459762c1ee843c1e576a11
wangy1b <wangy1b@163.com>
2020-09-04 15:38:18 +0800

    LongestUnivaluePath
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/LongestUnivaluePath.java
+++ src/main/java/com/wyb/leetcode/LongestUnivaluePath.java
@@ -41,7 +41,7 @@ public class LongestUnivaluePath {
         TreeNode treeNode = new TreeNode(
                 1
                 ,new TreeNode(4
-                            ,new TreeNode(4)
+                            ,new TreeNode(4,new TreeNode(4),null)
                             ,new TreeNode(4)
                                  )
                 ,new TreeNode(5,null,new TreeNode(5))
@@ -51,24 +51,50 @@ public class LongestUnivaluePath {
     }
     static int res = 0;
 
-    // TODO: 2020/9/3 not finished
     private static int longestUnivaluePath(TreeNode root) {
         if (root == null) return 0;
-        return Math.max(helper(root.left,root.val),helper(root.right,root.val));
+        helper(root);
+        return res;
     }
 
-    private static int helper(TreeNode node ,int val){
-        if (val == node.val) {
-            res ++;
-        }
-        else {
-            val = node.val;
-            res = 0;
-        }
+    /*
 
-        if (node.left != null) helper(node.left,val);
-        if (node.right != null) helper(node.right,val);
-        return res;
+    方法：递归
+    思路
+
+    我们可以将任何路径（具有相同值的节点）看作是最多两个从其根延伸出的箭头。
+
+    具体地说，路径的根将是唯一节点，因此该节点的父节点不会出现在该路径中，而箭头将是根在该路径中只有一个子节点的路径。
+
+    然后，对于每个节点，我们想知道向左延伸的最长箭头和向右延伸的最长箭头是什么？我们可以用递归来解决这个问题。
+
+    算法
+
+    令 arrow_length(node) 为从节点 node 延伸出的最长箭头的长度。如果 node.Left 存在且与节点 node 具有相同的值，
+    则该值就会是 1 + arrow_length(node.left)。
+    在 node.right 存在的情况下也是一样。
+
+    当我们计算箭头长度时，候选答案将是该节点在两个方向上的箭头之和。我们将这些候选答案记录下来，并返回最佳答案。
+
+    作者：LeetCode
+    链接：https://leetcode-cn.com/problems/longest-univalue-path/solution/zui-chang-tong-zhi-lu-jing-by-leetcode/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+     */
+    private static int helper(TreeNode node){
+        if (node == null) return 0;
+        int left = helper(node.left);
+        int right = helper(node.right);
+        int arrowLeft = 0, arrowRight = 0;
+        if (node.left != null && node.left.val == node.val) {
+            arrowLeft += left + 1;
+        }
+        if (node.right != null && node.right.val == node.val) {
+            arrowRight += right + 1;
+        }
+        res = Math.max(res, arrowLeft + arrowRight);
+        return Math.max(arrowLeft, arrowRight);
     }
 
 
