3510f9019e9104f6b5f5447644fa6b3b2aa0b23d
wangy1b <wangy1b@163.com>
2020-09-08 15:54:52 +0800

    DeleteNodeInABst
    Signed-off-by: wangy1b <wangy1b@163.com>

---
+++ src/main/java/com/wyb/leetcode/DeleteNodeInABst.java
@@ -0,0 +1,106 @@
+package com.wyb.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+
+450. 删除二叉搜索树中的节点
+给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+
+一般来说，删除节点可分为两个步骤：
+
+首先找到需要删除的节点；
+如果找到了，删除它。
+说明： 要求算法时间复杂度为 O(h)，h 为树的高度。
+
+示例:
+
+root = [5,3,6,2,4,null,7]
+key = 3
+
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+
+一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+
+    5
+   / \
+  4   6
+ /     \
+2       7
+
+另一个正确答案是 [5,2,6,null,4,null,7]。
+
+    5
+   / \
+  2   6
+   \   \
+    4   7
+
+https://leetcode-cn.com/problems/delete-node-in-a-bst/
+ */
+public class DeleteNodeInABst {
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(
+                5
+                ,new TreeNode(3
+                    ,new TreeNode(2)
+                    ,new TreeNode(4)
+                )
+                ,new TreeNode(6,null, new TreeNode(7))
+        );
+
+        TreeNode res = deleteNode(treeNode,3);
+    }
+
+    /*
+     One step right and then always left
+     */
+    private static int successor(TreeNode root) {
+        root = root.right;
+        while (root.left != null) root = root.left;
+        return root.val;
+    }
+
+    /*
+    One step left and then always right
+    */
+    private static int predecessor(TreeNode root) {
+        root = root.left;
+        while (root.right != null) root = root.right;
+        return root.val;
+    }
+
+    private static TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+
+        // delete from the right subtree
+        if (key > root.val) root.right = deleteNode(root.right, key);
+            // delete from the left subtree
+        else if (key < root.val) root.left = deleteNode(root.left, key);
+            // delete the current node
+        else {
+            // the node is a leaf
+            if (root.left == null && root.right == null) root = null;
+                // the node is not a leaf and has a right child
+            else if (root.right != null) {
+                root.val = successor(root);
+                root.right = deleteNode(root.right, root.val);
+            }
+            // the node is not a leaf, has no right child, and has a left child
+            else {
+                root.val = predecessor(root);
+                root.left = deleteNode(root.left, root.val);
+            }
+        }
+        return root;
+    }
+
+}
