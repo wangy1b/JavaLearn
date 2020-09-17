f29826b38f85ea1841f9b9ba4f564740c9f3b5eb
wangy1b <wangy1b@163.com>
2020-09-17 18:52:36 +0800

    Leetcode official
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/UniqueBinarySearchTreesII.java
+++ src/main/java/com/wyb/leetcode/UniqueBinarySearchTreesII.java
@@ -44,7 +44,7 @@ public class UniqueBinarySearchTreesII {
         List<TreeNode> res = generateTrees(n);
     }
 
-    private static List<TreeNode> generateTrees(int n) {
+    private static List<TreeNode> myGenerateTrees(int n) {
         if (n <= 0) return null;
         List<TreeNode> res = new ArrayList<>();
         // preorder
@@ -88,6 +88,42 @@ public class UniqueBinarySearchTreesII {
             helper(root.right, start + 1, end, mid);
         }
 
+    }
+
+
+    private static List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private static List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<TreeNode>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+
+            // 获得所有可行的右子树集合
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+            // 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode currTree = new TreeNode(i);
+                    currTree.left = left;
+                    currTree.right = right;
+                    allTrees.add(currTree);
+                }
+            }
+        }
+        return allTrees;
     }
 
 }
