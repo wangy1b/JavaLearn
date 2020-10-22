148e8b8e8516496cbcc0c8cf660a644e592889a3
wangy1b <wangy1b@163.com>
2020-10-22 17:36:03 +0800

    ReadCSV
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/IO/ReadCSV.java
+++ src/main/java/com/wyb/IO/ReadCSV.java
@@ -2,42 +2,65 @@ package com.wyb.IO;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ReadCSV {
     public static void main(String[] args) throws FileNotFoundException {
 
 
-        String filePath = "C:\\Users\\32006\\Desktop\\ZKSBM_20200829(1).csv";
-        File in =  new File(filePath);
-        encode();
-        InputStreamReader inputStreamReader = null;
+        String filePath = "C:\\Users\\32006\\Desktop\\YCustomer_20200902.csv";
+
         try {
-            inputStreamReader = new InputStreamReader(new FileInputStream(in),"GB18030");
-            System.out.println(inputStreamReader.getEncoding());
+            File fileDir = new File(filePath);
+            File outFileDir = new File(filePath+"new");
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFileDir), "utf8"));
 
-            char[] chars = new char[1024];
-            String content = "";
-            while (inputStreamReader.read(chars) > 0 ) {
-                content += new String( chars );
-                break;
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(fileDir), "utf-8"));
+
+
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                String tmp = new String(str.getBytes("gbk"));
+                System.out.println(tmp);
+                out.write(tmp+"\n");
             }
-            System.out.println(content);
 
-
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            in.close();
+            out.close();
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
         }
 
-
-        // new InputStreamReader(new FileInputStream(in), "ISO8859_1");
 
         // String str = "021000140510000;;0001;鑰佸簷瓒抽噾999鑿犺悵鑺辫矾璺€氳浆杩愮彔鎸傚潬100014;0001;榛勯噾楗板搧;0000000005;鎸傚潬;0000;;鏄?0000000002;瓒抽噾999;;;00000000;;";
         //
         // System.out.println(getUtf8String(str));
 
+
+        // System.out.println(new String("鑰佸簷鍢夊畾娓呮渤搴?;".getBytes("UTF-8"), "GBK"));
+        // try {
+        //     String str = "鑰佸簷鍢夊畾娓呮渤搴?;";
+        //     System.out.println(new String(str.getBytes("BIG5"),"UTF-8"));
+        //     System.out.println(new String(str.getBytes("GB18030"),"UTF-8"));
+        //
+        // } catch (UnsupportedEncodingException e) {
+        //     e.printStackTrace();
+        // }
 
     }
 
@@ -69,6 +92,66 @@ public class ReadCSV {
             byte[] utf8 = name.getBytes("UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void test(String fileSt) throws IOException {
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        String s;
+
+        try {
+            // new input stream reader is created
+            fis = new FileInputStream(fileSt);
+            isr = new InputStreamReader(fis);
+
+            // the name of the character encoding returned
+            s = isr.getEncoding();
+            System.out.print("Character Encoding: "+s);
+
+        } catch (Exception e) {
+            // print error
+            System.out.print("The stream is already closed");
+        } finally {
+            // closes the stream and releases resources associated
+            if(fis!=null)
+                fis.close();
+            if(isr!=null)
+                isr.close();
+        }
+    }
+
+
+    public static byte[] readBytes(InputStream inputStream) throws IOException {
+        byte[] b = new byte[1024];
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        int c;
+        while ((c = inputStream.read(b)) != -1) {
+            os.write(b, 0, c);
+            break;
+        }
+        return os.toByteArray();
+    }
+
+
+    public static void transform(File source, String srcEncoding, File target, String tgtEncoding) throws IOException {
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try{
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(source),srcEncoding));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(target), tgtEncoding));
+            char[] buffer = new char[16384];
+            int read;
+            while ((read = br.read(buffer)) != -1)
+                bw.write(buffer, 0, read);
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } finally {
+                if (bw != null)
+                    bw.close();
+            }
         }
     }
 }
