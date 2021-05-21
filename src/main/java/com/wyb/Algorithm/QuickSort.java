e7ba8f2819cd6f83c60e9aaf55b6c568cd656718
wangy1b <wangy1b@163.com>
2021-05-21 17:21:17 +0800

    add doc
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/Algorithm/QuickSort.java
+++ src/main/java/com/wyb/Algorithm/QuickSort.java
@@ -8,6 +8,15 @@ public class QuickSort {
         printArr(arr);
     }
 
+    /**
+     * 快速排序（Quicksort）是对冒泡排序算法的一种改进
+     * 基本思想是：通过一趟排序将要排序的数据分割成独立的两部分，其中一部分的所有数据都比另外一部分的所有数据都要小，
+     * 然后再按此方法对这两部分数据分别进行快速排序，整个排序过程可以递归进行，以此达到整个数据变成有序序列。
+     *
+     * @param arr
+     * @param leftBound
+     * @param rightBound
+     */
     public static void sort(int[] arr, int leftBound, int rightBound){
         if(leftBound >= rightBound) return;
 
@@ -18,6 +27,10 @@ public class QuickSort {
         sort(arr,mid + 1, rightBound );
     }
 
+    /**
+     *  从两边往中间同时找，从左找到第一个比轴大的值，从右找到第一个比轴小的值，
+     *  找到之后，这两个数直接做交换
+     */
     private static int partition(int[] arr, int leftBound, int rightBound){
         int pivot = arr[rightBound];
         int left = leftBound;
