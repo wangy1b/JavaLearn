e24851761be74d7d33432c108ed5175d4a076436
wangy1b <wangy1b@163.com>
2021-05-21 11:41:36 +0800

    rm wrong solution
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/Algorithm/BubbleSort.java
+++ src/main/java/com/wyb/Algorithm/BubbleSort.java
@@ -7,22 +7,7 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] arr = {5, 3, 6, 8, 1, 7, 9, 4, 2, 10};
-        //mySort(arr);
         sort(arr);
-    }
-
-
-    public static void mySort(int[] arr){
-
-        for (int i = 0; i < arr.length -1 ; i++) {
-            for (int j = i + 1; j < arr.length -1   ; j++) {
-                if (arr[i] > arr[j]) {
-                    swap(arr, i, j);
-                }
-            }
-        }
-        //System.out.println("排序结果为：");
-        //printArr(arr);
     }
 
     public static void sort(int[] arr){
