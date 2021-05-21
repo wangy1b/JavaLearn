c2f6eb109c7feb7983b8eba5ab82191b8c432bc2
wangy1b <wangy1b@163.com>
2021-05-21 11:23:38 +0800

    add Official solutions
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/TopKFrequentWords.java
+++ src/main/java/com/wyb/leetcode/TopKFrequentWords.java
@@ -40,7 +40,9 @@ public class TopKFrequentWords {
         // String[] words = {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
         // int k = 4;
         TopKFrequentWords topKF = new TopKFrequentWords();
-        List<String> res = topKF.topKFrequent(words, k);
+        // List<String> res = topKF.topKFrequent(words, k);
+        // List<String> res = topKF.topKFrequentOfficial(words, k);
+        List<String> res = topKF.topKFrequentOfficialPQ(words, k);
 
         System.out.println(res);
     }
@@ -82,5 +84,63 @@ public class TopKFrequentWords {
             res[k - 1 - i] = heap.poll().getKey();
         }
         return Arrays.asList(res);
+    }
+
+    //方法一：哈希表 + 排序
+    // 思路及算法
+    //
+    // 我们可以预处理出每一个单词出现的频率，然后依据每个单词出现的频率降序排序，最后返回前 k 个字符串即可。
+    //
+    // 具体地，我们利用哈希表记录每一个字符串出现的频率，然后将哈希表中所有字符串进行排序，
+    // 排序时，如果两个字符串出现频率相同，那么我们让两字符串中字典序较小的排在前面，否则我们让出现频率较高的排在前面。
+    // 最后我们只需要保留序列中的前 k 个字符串即可。
+    //
+    // 作者：LeetCode-Solution
+    // 链接：https://leetcode-cn.com/problems/top-k-frequent-words/solution/qian-kge-gao-pin-dan-ci-by-leetcode-solu-3qk0/
+    // 来源：力扣（LeetCode）
+    // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    public List<String> topKFrequentOfficial(String[] words, int k) {
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        }
+        List<String> rec = new ArrayList<String>();
+        for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
+            rec.add(entry.getKey());
+        }
+        Collections.sort(rec, new Comparator<String>() {
+            public int compare(String word1, String word2) {
+                return cnt.get(word1) == cnt.get(word2) ? word1.compareTo(word2) : cnt.get(word2) - cnt.get(word1);
+            }
+        });
+        return rec.subList(0, k);
+    }
+
+
+    public List<String> topKFrequentOfficialPQ(String[] words, int k) {
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<Map.Entry<String, Integer>>(
+                new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                return entry1.getValue() == entry2.getValue() ?
+                        entry2.getKey().compareTo(entry1.getKey()) :
+                        entry1.getValue() - entry2.getValue();
+            }
+        });
+        for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
+            pq.offer(entry);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        List<String> ret = new ArrayList<String>();
+        while (!pq.isEmpty()) {
+            ret.add(pq.poll().getKey());
+        }
+        Collections.reverse(ret);
+        return ret;
     }
 }
