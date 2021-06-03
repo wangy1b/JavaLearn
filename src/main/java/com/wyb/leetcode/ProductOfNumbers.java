71e473800a28df61f80cab7dc7624b7bdb845752
wangy1b <wangy1b@163.com>
2021-06-03 11:12:41 +0800

    ProductOfNumbers 每次新增的时候，把从非0开始到当前前一个位置的数据都乘以新增的num，返回的是后直接取index-k的值即可，但是运行比之前更慢了
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/ProductOfNumbers.java
+++ src/main/java/com/wyb/leetcode/ProductOfNumbers.java
@@ -42,6 +42,7 @@ public class ProductOfNumbers {
     public int index = 0;
     private int size = 0;
     private int default_size = 10;
+    private int start_index = 0;
 
     private void grow() {
         int new_size = size + (size >> 1);
@@ -59,35 +60,39 @@ public class ProductOfNumbers {
     public void add(int num) {
         if (index == size)
             grow();
+        // 每次增加就把最后一个值跟前面所有的数据都乘一遍
+        // 包含0 之前的都已经全是0了，不需要再去遍历
+        for (int i = start_index; i < index; i++) {
+            data[i] *= num;
+        }
+        // 赋值当前值
         data[index++] = num;
+        // 记录包含0 的下一个位置
+        if (num == 0)
+            start_index = index;
     }
 
     public int getProduct(int k) {
         if (k <= 0) return 0;
-        int res = 1;
-        for (int i = index - 1; i > index - k - 1; i--) {
-            if (data[i] == 0)
-                return 0;
-            res *= data[i];
-        }
-        return res;
+        // 返回第index - k 个的值
+        return data[index - k];
     }
 
 
     public static void main(String[] args) {
         ProductOfNumbers productOfNumbers = new ProductOfNumbers();
-        // productOfNumbers.add(3);        // [3]
-        // productOfNumbers.add(0);        // [3,0]
-        // productOfNumbers.add(2);        // [3,0,2]
-        // productOfNumbers.add(5);        // [3,0,2,5]
-        // productOfNumbers.add(4);        // [3,0,2,5,4]
-        // System.out.println(productOfNumbers.getProduct(2)); // 返回 20 。最后 2 个数字的乘积是 5 * 4 = 20
-        // System.out.println(productOfNumbers.getProduct(3)); // 返回 40 。最后 3 个数字的乘积是 2 * 5 * 4 = 40
-        // System.out.println(productOfNumbers.getProduct(4)); // 返回  0 。最后 4 个数字的乘积是 0 * 2 * 5 * 4 = 0
-        // productOfNumbers.add(8);        // [3,0,2,5,4,8]
-        // System.out.println(productOfNumbers.getProduct(2)); // 返回 32 。最后 2 个数字的乘积是 4 * 8 = 32
-        for (int i = 0; i < 32; i++) {
-            productOfNumbers.add(i);
-        }
+        productOfNumbers.add(3);        // [3]
+        productOfNumbers.add(0);        // [3,0]
+        productOfNumbers.add(2);        // [3,0,2]
+        productOfNumbers.add(5);        // [3,0,2,5]
+        productOfNumbers.add(4);        // [3,0,2,5,4]
+        System.out.println(productOfNumbers.getProduct(2)); // 返回 20 。最后 2 个数字的乘积是 5 * 4 = 20
+        System.out.println(productOfNumbers.getProduct(3)); // 返回 40 。最后 3 个数字的乘积是 2 * 5 * 4 = 40
+        System.out.println(productOfNumbers.getProduct(4)); // 返回  0 。最后 4 个数字的乘积是 0 * 2 * 5 * 4 = 0
+        productOfNumbers.add(8);        // [3,0,2,5,4,8]
+        System.out.println(productOfNumbers.getProduct(2)); // 返回 32 。最后 2 个数字的乘积是 4 * 8 = 32
+        // for (int i = 0; i < 32; i++) {
+        //     productOfNumbers.add(i);
+        // }
     }
 }
