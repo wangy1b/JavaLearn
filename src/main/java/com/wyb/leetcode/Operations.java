20f22d85ca46ff826cfbb1ea12601d8cf11b0042
wangy1b <wangy1b@163.com>
2021-06-07 17:43:53 +0800

    参考 作者：sskbskdrin 链接：https://leetcode-cn.com/problems/operations-lcci/solution/mian-shi-ti-1609-yun-suan-zhi-yong-liao-jia-fa-yu-/
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/Operations.java
+++ src/main/java/com/wyb/leetcode/Operations.java
@@ -27,65 +27,90 @@ package com.wyb.leetcode;
 
  */
 class Operations {
+    // -1
+    int ne = Integer.MAX_VALUE + Integer.MAX_VALUE + 1;
+    // 1的n次方，1,2,4,8...
+    long[] poCache = new long[32];
+    // -1的n次方，-1,-2,-4,-8...
+    long[] neCache = new long[32];
+    long[] cache = new long[32]; // 存放乘数或除数的倍数，1*a,2*a,4*a,8*a...主要用于快速计算，不然容易超时
+    long[] cache1 = new long[32]; // 存放乘数或除数的倍数 负数-1*a,-2*a,-4*a,-8*a
+
 
     public Operations() {
-
-    }
-
-    /**
-     * 取相反数
-     * @param s
-     * @return
-     */
-    private int negate(int s) {
-        int res = 0;
-        int flag = s > 0 ? 1 : -1;
-        while (s != 0) {
-            // 结果加flag
-            res += flag;
-            // 判断条件加flag
-            s += flag;
+        poCache[0] = 1;
+        neCache[0] = ne;
+        for (int i = 1; i < 32; i++) {
+            poCache[i] = poCache[i + ne] + poCache[i + ne];
+            neCache[i] = neCache[i + ne] + neCache[i + ne];
         }
-        return res;
     }
 
-    /**
-     * 取绝对值
-     * @param s
-     * @return
-     */
-    private int abs(int s){
-        if (s >= 0) return s;
-        return negate(s);
-    }
-
-    // 超出时间限制
     // minus(a, b) 减法，返回a - b
     public int minus(int a, int b) {
-        return a + negate(b);
+        if (a == b) return 0;
+        int index = 31;
+        // a - b = res =>  (a - b) + (b - b) = res => 让ab同幅度递减，只要让b减到0，那res = a 剩下部分
+        while ( b != 0) {
+            // b 为正数
+            if (b > 0) {
+                if (b >= poCache[index]) { // 如果b大于2的index次方，
+                    b += neCache[index];
+                    a += neCache[index];
+                } else {
+                    index += ne;
+                }
+            } else { // b 为负数
+                if (b <= neCache[index]) {
+                    b += poCache[index];
+                    a += poCache[index];
+                } else {
+                    index += ne;
+                }
+            }
+        }
+        return a;
     }
 
     // multiply(a, b) 乘法，返回a * b
     // 转化为a个b相加(当a<b时)，注意符号就行
     public int multiply(int a, int b) {
         if (a == 0 || b == 0) return 0;
-        int flag = 1;
-        if ( a >= 0 && b >= 0) {
-            flag = 1;
-        }
-        else {
-            flag = -1;
-            a = abs(a);
-            b = abs(b);
+        if (a == 1) return b;
+        if (b == 1) return a;
+        if (a == ne) return minus(0, b);
+        if (b == ne) return minus(0, a);
+        int sign = (a > 0 && b > 0) || (a < 0 && b < 0) ? 1 : ne;
+        // 把b变成正数
+        if (b < 0) {
+            b = minus(0, b);
         }
 
-        int maxOne = a > b ? a : b;
-        int minOne = a > b ? b : a;
-        int res = 0;
-        for (int i = 0; i < minOne; i++) {
-            res += maxOne;
+        cache[0] = a;
+        for (int i = 1; i < 32; i++) {
+            cache[i] = cache[i + ne] + cache[i + ne];
         }
-        return flag > 0 ? res : negate(res);
+        int index = 30; // 从31开始应该也是可以的
+        int ret = 0;
+        int retSign = a > 0 ? 1 : ne; // 记录返回值的符号
+        while (b > 0) {
+            if (b >= poCache[index]) {
+                b += neCache[index];
+                ret += cache[index];
+                retSign = ret > 0 ? 1 : ne;// 记录返回值的符号
+            } else {
+                index += ne;
+            }
+        }
+        // 根据初始值改变返回值的符号
+        if ((sign < 0 && ret > 0) || (sign > 0 && ret < 0)) {
+            ret = minus(0, ret);
+        }
+        // 结果溢出，返回值的符号会变成相反的
+        if (retSign != (a > 0 ? 1 : ne)) {
+            ret = minus(0, ret);
+        }
+        return ret;
     }
 
     // divide(a, b) 除法，返回a / b
@@ -93,28 +118,54 @@ class Operations {
     public int divide(int a, int b) {
         // 被除数不能为0
         assert b != 0;
-        // 正负值判断条件
-        int flag = 1;
-        if ( a >= 0 && b > 0) {
-            flag = 1;
+        if (a == 0) return 0;
+        if (b == 1) return a;
+        if (b == ne) return minus(0, a);
+        int ret = 0;
+        int sign = (a > 0 && b > 0) || (a < 0 && b < 0) ? 1 : ne;
+        long nb = b;
+        long pb = b;
+        if (b < 0) {
+            b = minus(0, b);
+        } else {
+            nb = minus(0, b);
         }
-        else {
-            flag = -1;
-            a = abs(a);
-            b = abs(b);
+        if (a < 0) {
+            a = minus(0, a);
         }
-        int res = 0;
-        while (multiply(res, b) < a) {
-                res += 1 ;
+        cache[0] = b;
+        cache1[0] = nb;
+        int index = 1;
+        for (; index < 32; ++index) {
+            cache[index] = cache[index + ne] + cache[index + ne];
+            cache1[index] = cache1[index + ne] + cache1[index + ne];
+            if (cache1[index] >= a) {
+                break; // 找到最大值就可以返回了，不用计算完
+            }
         }
-        return flag > 0 ? res : negate(res);
+        if (index >= 32) index = 31;
+        while (a >= b) {
+            if (a >= cache[index]) {
+                ret += poCache[index];// 注意这里是2的index次方的值
+                a += cache1[index];
+            } else {
+                index += ne;
+            }
+        }
+        if (sign < 0) {
+            ret = minus(0, ret);
+        }
+        return ret;
     }
 
     public static void main(String[] args) {
         Operations operations = new Operations();
-        // System.out.println(operations.minus(1, 2)); //返回-1
-        System.out.println(operations.minus(10,-2147483647)); //返回-1
-        System.out.println(operations.multiply(3, 4)); //返回12
-        System.out.println(operations.divide(5, -3)); //返回-2
+        System.out.println(operations.minus(1, 2)); //返回-1
+        // System.out.println(operations.minus(10,2147483647)); //返回-1
+        // System.out.println(operations.multiply(3, 4)); //返回12
+        // long t1 = System.nanoTime();
+        // System.out.println(operations.divide(5, -3)); //返回-2
+        // long t2 = System.nanoTime();
+        // System.out.println("divide用时："+(t2 - t1)/1000000000 + "s");
     }
 }
