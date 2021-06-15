411bd91e512b7348125d2c928a93b804a73365d0
wangy1b <wangy1b@163.com>
2021-06-15 16:52:03 +0800

    插入排序 + 二分法寻找插入地址
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/FindMedianFromDataStreamI.java
+++ src/main/java/com/wyb/leetcode/FindMedianFromDataStreamI.java
@@ -33,24 +33,23 @@ import java.util.*;
 
 
 /**
- 方法二： 插入排序
-
- 保持输入容器始终排序
-
- 算法：
- 哪种算法允许将一个数字添加到已排序的数字列表中，但仍保持整个列表的排序状态？插入排序！
-
- 我们假设当前列表已经排序。当一个新的数字出现时，我们必须将它添加到列表中，同时保持列表的排序性质。
- 这可以通过使用二分搜索找到插入传入号码的正确位置来轻松实现。（记住，列表总是排序的）。
- 一旦找到位置，我们需要将所有较高的元素移动一个空间，以便为传入的数字腾出空间。
-
- 当插入查询的数量较少或者中间查找查询的数量大致相同。 此方法会很好地工作。
-
- 作者：LeetCode
- 链接：https://leetcode-cn.com/problems/find-median-from-data-stream/solution/shu-ju-liu-de-zhong-wei-shu-by-leetcode/
- 来源：力扣（LeetCode）
- 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-
+ * 方法二： 插入排序
+ * <p>
+ * 保持输入容器始终排序
+ * <p>
+ * 算法：
+ * 哪种算法允许将一个数字添加到已排序的数字列表中，但仍保持整个列表的排序状态？插入排序！
+ * <p>
+ * 我们假设当前列表已经排序。当一个新的数字出现时，我们必须将它添加到列表中，同时保持列表的排序性质。
+ * 这可以通过使用二分搜索找到插入传入号码的正确位置来轻松实现。（记住，列表总是排序的）。
+ * 一旦找到位置，我们需要将所有较高的元素移动一个空间，以便为传入的数字腾出空间。
+ * <p>
+ * 当插入查询的数量较少或者中间查找查询的数量大致相同。 此方法会很好地工作。
+ * <p>
+ * 作者：LeetCode
+ * 链接：https://leetcode-cn.com/problems/find-median-from-data-stream/solution/shu-ju-liu-de-zhong-wei-shu-by-leetcode/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 
 public class FindMedianFromDataStreamI {
@@ -67,28 +66,28 @@ public class FindMedianFromDataStreamI {
         size = 0;
     }
 
-    private void grow(){
+    private void grow() {
         if (size == store.length) {
-            int newSize = size + (size >> 1 );
+            int newSize = size + (size >> 1);
             int[] newStore = new int[newSize];
-            System.arraycopy(store,0,newStore,0,size);
+            System.arraycopy(store, 0, newStore, 0, size);
             store = newStore;
         }
     }
 
-    private int binarySearchIdx(int num,int start, int end){
-        if (start == end && num >= store[start]) return start+1;
+    private int binarySearchIdx(int num, int start, int end) {
+        if (start == end && num >= store[start]) return start + 1;
         if (start == end && num < store[start]) return start;
         int medianIdx = (start + end) >> 1;
         if (num > store[medianIdx])
-            return binarySearchIdx(num,medianIdx+1,end);
+            return binarySearchIdx(num, medianIdx + 1, end);
         else if (num < store[medianIdx])
-            return binarySearchIdx(num,0,medianIdx);
-        return medianIdx +1;
+            return binarySearchIdx(num, 0, medianIdx);
+        return medianIdx + 1;
     }
 
     public void addNum(int num) {
-        int moveIdx = size == 0 ? 0 : binarySearchIdx(num,0,size - 1);
+        int moveIdx = size == 0 ? 0 : binarySearchIdx(num, 0, size - 1);
         int moveNums = size - 1 - moveIdx + 1;
         if (moveNums > 0) {
             // 拷贝后半段
