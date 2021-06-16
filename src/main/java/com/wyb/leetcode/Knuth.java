b44b2e69a6fed160c9c99eeb07495244fc23b6b4
wangy1b <wangy1b@163.com>
2021-06-16 11:24:14 +0800

    code format
    Signed-off-by: wangy1b <wangy1b@163.com>

--- src/main/java/com/wyb/leetcode/Knuth.java
+++ src/main/java/com/wyb/leetcode/Knuth.java
@@ -38,11 +38,10 @@ public class Knuth {
 
     // 算法，可以保证上面我所说的，对于生成的排列，每一个元素都能等概率的出现在每一个位置。
     // 或者反过来，每一个位置都能等概率的放置每个元素。
-    public static void randomize( int arr[], int n)
-    {
+    public static void randomize(int arr[], int n) {
         Random r = new Random();
-        for (int i = n-1; i > 0; i--) {
-            int j = r.nextInt(i+1); // 选择一个 0,i+1 的随机数
+        for (int i = n - 1; i > 0; i--) {
+            int j = r.nextInt(i + 1); // 选择一个 [0,i] 的随机数
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
@@ -64,6 +63,6 @@ public class Knuth {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = arr.length;
         // randomize (arr, n);
-        randomize (a, 52);
+        randomize(a, 52);
     }
 }
